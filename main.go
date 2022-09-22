14b732bf40e31e29dc3fa24700c1d3f44170aedd
Marcos Travasso <marcos.travasso42@gmail.com>
2022-09-22 19:10:51 -0300

    :sparkles: Add anti-lock function

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 type argT struct {
 	Endpoint string `cli:"*e,endpoint" usage:"URL with the endpoint used for check"`
 	Tries    int    `cli:"t,tries" usage:"number of tries before fail" dft:"10"`
-	Delay    int64  `cli:"d,delay" usage:"delay between tries (in seconds)" dft:"20"`
+	Delay    int    `cli:"d,delay" usage:"delay between tries (in seconds)" dft:"20"`
 }
 
 var ARGS *argT
@@ -19,6 +19,7 @@ func main() {
 	GetArgs()
 
 	log.Printf("Requesting at %s\n", FormatURL(ARGS.Endpoint))
+	go AntiLock()
 
 	for i := 0; i < ARGS.Tries; i++ {
 		log.Printf("#%d request\n", i+1)
@@ -41,4 +42,10 @@ func GetArgs() {
 	}) == 1 {
 		os.Exit(1)
 	}
+}
+
+func AntiLock() {
+	time.Sleep(time.Duration(ARGS.Delay*ARGS.Tries) * time.Second)
+	log.Printf("REQUESTS LOCKED")
+	os.Exit(1)
 }
